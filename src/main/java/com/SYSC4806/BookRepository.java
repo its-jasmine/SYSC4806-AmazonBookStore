252a85d38a8b330d5b0985b236f1a6cd3fdbcbb4
nivetha <nive0204@gmail.com>
2024-11-10 09:58:18 -0500

    added frontend for book management

--- src/main/java/com/SYSC4806/BookRepository.java
+++ src/main/java/com/SYSC4806/BookRepository.java
@@ -3,9 +3,13 @@ package com.SYSC4806;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
 
     Iterable<Object> findByTitle(String title);
+
+    Optional<Book> findByTitleAndAuthor(String title, String author);
 }
 
