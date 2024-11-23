360f72e2ebce4a4cb82c8ea0bb11148ea6e64f81
Jasmine Gad El Hak <itsjessyg@gmail.com>
2024-11-22 23:50:16 -0500

    Improving controller tests

--- src/test/java/com/SYSC4806/BookFilteringControllerTest.java
+++ src/test/java/com/SYSC4806/BookFilteringControllerTest.java
@@ -8,6 +8,9 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -31,22 +34,46 @@ class BookFilteringControllerTest {
 
     @Test
     void showFilteredBooksByGenrePage() throws Exception {
-        when(bookRepository.findByGenre(Book.Genre.Fiction)).thenReturn(new ArrayList<Book>());
+        Book testBook = new Book("1234567890","Test Title", "Test Author", "Test Publisher", 19.99, Book.Genre.Fiction, 10);
+        List<Book> genreBooks = List.of(testBook);
+
+        when(bookRepository.findByGenre(Book.Genre.Fiction)).thenReturn(genreBooks);
 
         // Verifying that accessing the book details url will successfully show the browse by genre page
         mockMvc.perform(get("/browse-by-genre").param("genre", Book.Genre.Fiction.name()))
                 .andExpect(status().isOk())
-                .andExpect(view().name("filtered-books-page"));
+                .andExpect(view().name("filtered-books-page"))
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attribute("books", genreBooks))
+                .andExpect(model().attribute("filter", Book.Genre.Fiction));
     }
 
     @Test
     void showSearchResultsPage() throws Exception {
+        Book titleBook = new Book("1234567890", "My Query", "Author 1", "Publisher 1", 19.99, Book.Genre.NonFiction, 5);
+        Book authorBook = new Book("0987654321", "Different Title", "My Query", "Publisher 2", 15.99, Book.Genre.Fiction, 3);
+
+        List<Book> titleBooks = List.of(titleBook);
+        List<Book> authorBooks = List.of(authorBook);
+
+        when(bookRepository.findByTitleContainingIgnoreCase("My Query")).thenReturn(titleBooks);
+        when(bookRepository.findByAuthorContainingIgnoreCase("My Query")).thenReturn(authorBooks);
+
+        List<Book> expectedBooks = Stream.concat(titleBooks.stream(), authorBooks.stream())
+                .collect(Collectors.toList());
+
+        Book testBook = new Book("1234567890","Test Title", "Test Author", "Test Publisher", 19.99, Book.Genre.Fiction, 10);
+        List<Book> genreBooks = List.of(testBook);
+
         when(bookRepository.findByTitle("My Query")).thenReturn(new ArrayList<Book>());
 
         // Verifying that accessing the book details url will successfully show the search results page
         mockMvc.perform(get("/search").param("query", "My Query"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("filtered-books-page"));
+                .andExpect(view().name("filtered-books-page"))
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attribute("books", expectedBooks))
+                .andExpect(model().attribute("filter", "My Query"));
     }
 
 }
